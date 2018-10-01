0cb5abcca378d6819eaa115b55791f1e369c4a9c
Piero <piero.vasquez.r@gmail.com>
2018-10-01 14:22:42 +0200

    LogicaNegocio por terminar

---
+++ libroelectronico/src/main/java/com/ipartek/formacion/libroelectronico/logica/LogicaNegocio.java
@@ -0,0 +1,40 @@
+package com.ipartek.formacion.libroelectronico.logica;
+
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+import com.ipartek.formacion.libroelectronico.pojo.Pagina;
+
+public class LogicaNegocio {
+
+	private static TreeMap<Integer, Pagina> libro = new TreeMap<>();
+	
+	static {
+		libro.put(new Integer(1), new Pagina(1,"YO", "Erase una vez ..."));
+	}
+	
+	public static Pagina obtenerPagina(int numeroPagina) {
+		if (numeroPagina < 1) {
+			numeroPagina = 1;
+		}
+		
+		if (numeroPagina > libro.size()) {
+			numeroPagina = libro.size();
+		}
+		
+		return libro.get(numeroPagina);
+	}
+	
+	public static int obtenerNumeroPaginas() {
+		return libro.size();
+	}
+	
+	public static boolean agregarPagina(Pagina pagina) {
+		if (new StringTokenizer(pagina.getContenido()).countTokens() >= 25) {
+			libro.put(pagina.getNumero(), pagina);
+			return true;
+		}else {
+			return false;
+		}
+	}
+}
